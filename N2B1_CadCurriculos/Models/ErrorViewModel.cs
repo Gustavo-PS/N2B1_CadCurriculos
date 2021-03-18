f2561e46f51a1807ffa786ef2c62250a7a72223a
GUSTAVO PEPINELLI SALGADO <gustavopepinelli@hotmail.com>
2021-03-18 11:25:56 -0300

    Criação do projeto e do repositório

---
+++ N2B1_CadCurriculos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace N2B1_CadCurriculos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
