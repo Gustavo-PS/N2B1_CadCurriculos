a13656fd522e49c194b0e37ba19c5c1de2bf059b
GUSTAVO PEPINELLI SALGADO <gustavopepinelli@hotmail.com>
2021-03-22 09:47:53 -0300

    Criação de classes, models e controllers

---
+++ N2B1_CadCurriculos/DAO/CurriculoDAO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace N2B1_CadCurriculos.DAO
+{
+    public class CurriculoDAO
+    {
+    }
+}
