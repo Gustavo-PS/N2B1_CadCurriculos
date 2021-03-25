f86c39934461e0afaecb0aa3734aa8afe45f946d
GUSTAVO PEPINELLI SALGADO <gustavopepinelli@hotmail.com>
2021-03-24 23:34:25 -0300

    asp-for

--- N2B1_CadCurriculos/Models/CurriculoViewModel.cs
+++ N2B1_CadCurriculos/Models/CurriculoViewModel.cs
@@ -7,79 +7,82 @@ namespace N2B1_CadCurriculos.Models
 {
     public class CurriculoViewModel
     {
-        public string nome { get; set; }
-        public string cpf { get; set; }
-        public string telefone { get; set; }
-        public string email { get; set; }
-        public string cargo { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Cargo { get; set; }
 
         //Endereço
-        public int cep { get; set; }
-        public string estado { get; set; }
-        public string cidade { get; set; }
-        public string bairo { get; set; }
-        public string rua { get; set; }
-        public int numero { get; set; }
+        public int Cep { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Rua { get; set; }
+        public int Numero { get; set; }
 
         //Dados de formação acadêmica e cursos
-        public string escolaridade1 { get; set; }
-        public string instituicao1 { get; set; }
-        public string situacao1 { get; set; }
-        public DateTime conclusao1 { get; set; }
-        public string periodo1 { get; set; }
+        public string Curso1 { get; set; }
+        public string Escolaridade1 { get; set; }
+        public string Instituicao1 { get; set; }
+        public string Situacao1 { get; set; }
+        public DateTime Conclusao1 { get; set; }
+        public string Periodo1 { get; set; }
 
-        public string escolaridade2 { get; set; }
-        public string instituicao2 { get; set; }
-        public string situacao2 { get; set; }
-        public DateTime conclusao2 { get; set; }
-        public string periodo2 { get; set; }
+        public string Curso2 { get; set; }
+        public string Escolaridade2 { get; set; }
+        public string Instituicao2 { get; set; }
+        public string Situacao2 { get; set; }
+        public DateTime Conclusao2 { get; set; }
+        public string Periodo2 { get; set; }
 
-        public string escolaridade3 { get; set; }
-        public string instituicao3 { get; set; }
-        public string situacao3 { get; set; }
-        public DateTime conclusao3 { get; set; }
-        public string periodo3 { get; set; }
+        public string Curso3 { get; set; }
+        public string Escolaridade3 { get; set; }
+        public string Instituicao3 { get; set; }
+        public string Situacao3 { get; set; }
+        public DateTime Conclusao3 { get; set; }
+        public string Periodo3 { get; set; }
 
-        public string escolaridade4 { get; set; }
-        public string instituicao4 { get; set; }
-        public string situacao4 { get; set; }
-        public DateTime conclusao4 { get; set; }
-        public string periodo4 { get; set; }
+        public string Curso4 { get; set; }
+        public string Escolaridade4 { get; set; }
+        public string Instituicao4 { get; set; }
+        public string Situacao4 { get; set; }
+        public DateTime Conclusao4 { get; set; }
+        public string Periodo4 { get; set; }
 
-        public string escolaridade5 { get; set; }
-        public string instituicao5 { get; set; }
-        public string situacao5 { get; set; }
-        public DateTime conclusao5 { get; set; }
-        public string periodo5 { get; set; }
+        public string Curso5 { get; set; }
+        public string Escolaridade5 { get; set; }
+        public string Instituicao5 { get; set; }
+        public string Situacao5 { get; set; }
+        public DateTime Conclusao5 { get; set; }
+        public string Periodo5 { get; set; }
 
         //Dados de experiências profissionais anteriores
-        public string empresa1 { get; set; }
-        public DateTime inicio1 { get; set; }
-        public DateTime termino1 { get; set; }
-        public int ocupacao1 { get; set; }
-        public string atividades1 { get; set; }
+        public string Empresa1 { get; set; }
+        public DateTime Inicio1 { get; set; }
+        public DateTime Termino1 { get; set; }
+        public string Ocupacao1 { get; set; }
+        public string Atividades1 { get; set; }
 
-        public string empresa2 { get; set; }
-        public DateTime inicio2 { get; set; }
-        public DateTime termino2 { get; set; }
-        public int ocupacao2 { get; set; }
-        public string atividades2 { get; set; }
+        public string Empresa2 { get; set; }
+        public DateTime Inicio2 { get; set; }
+        public DateTime Termino2 { get; set; }
+        public string Ocupacao2 { get; set; }
+        public string Atividades2 { get; set; }
 
-        public string empresa3 { get; set; }
-        public DateTime inicio3 { get; set; }
-        public DateTime termino3 { get; set; }
-        public int ocupacao3 { get; set; }
-        public string atividades3 { get; set; }
+        public string Empresa3 { get; set; }
+        public DateTime Inicio3 { get; set; }
+        public DateTime Termino3 { get; set; }
+        public string Ocupacao3 { get; set; }
+        public string Atividades3 { get; set; }
 
 
         //Dados de idiomas
-        public int idioma1 { get; set; }
-        public int nivelidiota1 { get; set; }
-
-        public int idioma2 { get; set; }
-        public int nivelidiota2 { get; set; }
-
-        public int idioma3 { get; set; }
-        public int nivelidiota3 { get; set; }
+        public int Idioma1 { get; set; }
+        public int Nivelidioma1 { get; set; }
+        public int Idioma2 { get; set; }
+        public int Nivelidioma2 { get; set; }
+        public int Idioma3 { get; set; }
+        public int Nivelidioma3 { get; set; }
     }
 }
