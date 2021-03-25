57c37597002b4c866faa44c15121a096b3ac7661
MARCIO RIOS KUNIMURA <marcio_rios_kunimura@hotmail.com>
2021-03-24 23:36:30 -0300

    CurriculoDAO incompleto

--- N2B1_CadCurriculos/DAO/CurriculoDAO.cs
+++ N2B1_CadCurriculos/DAO/CurriculoDAO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using N2B1_CadCurriculos.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,31 @@ namespace N2B1_CadCurriculos.DAO
 {
     public class CurriculoDAO
     {
+
+        private SqlParameter[] CriaParametros(CurriculoViewModel c)
+        {
+            SqlParameter[] parametros = new SqlParameter[5];
+            parametros[0] = new SqlParameter("cpf", c.cpf);
+            parametros[1] = new SqlParameter("nome", c.nome);
+            parametros[2] = new SqlParameter("telefone", c.telefone);
+            parametros[3] = new SqlParameter("email", c.email);
+            parametros[4] = new SqlParameter("cargo", c.cargo);
+
+            parametros[5] = new SqlParameter("cep", c.cep);
+            parametros[6] = new SqlParameter("estado", c.estado);
+            parametros[7] = new SqlParameter("cidade", c.cidade);
+            parametros[8] = new SqlParameter("bairro", c.bairro);
+            parametros[9] = new SqlParameter("rua", c.rua);
+            parametros[10] = new SqlParameter("numero", c.numero);
+
+            return parametros;
+        }
+        public void Inserir(CurriculoViewModel c)
+        {
+            string sql =
+            "insert into alunos(id, nome, mensalidade, cidadeId, dataNascimento)" +
+            "values ( @id, @nome, @mensalidade, @cidadeId, @dataNascimento)";
+            HelperDAO.ExecutaSQL(sql, CriaParametros(c));
+        }
     }
 }
